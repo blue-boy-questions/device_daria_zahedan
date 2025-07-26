f0ea61a58816b15f5282fd24dea7b2b32e549f00
Dhina17 <dhinalogu@gmail.com>
2025-07-26 22:45:01 +0530

    zahedan: Set lineage health config via soong select()

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/devices/platform/charger/cmd_charge_disable)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.zahedan
