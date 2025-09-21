615d6868b195b3527689ff097e42fb8801bc6176
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-09-21 09:43:15 +0330

    Revert "zahedan: Build EdgeCutout RRO"
    This reverts commit a93218fc82b3ede4208e1eeca3b85d785d4f8a74.
    
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- device.mk
+++ device.mk
@@ -329,7 +329,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
-    LMODroidEdgeCutout \
     FrameworksResOverlayZahedan \
     SettingsOverlayZahedan \
     SystemUIOverlayZahedan \
