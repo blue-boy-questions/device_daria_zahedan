69046dec5c6a8337b0df46d45e418014768d9072
Farzin Kazemzadeh <itisFarzin@gmail.com>
2025-09-25 10:16:47 +0330

    zahedan: bluetooth: Build AIDL instead of HIDL
    Signed-off-by: Farzin Kazemzadeh <itisFarzin@gmail.com>

--- device.mk
+++ device.mk
@@ -83,8 +83,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
-    android.hardware.bluetooth.audio@2.1-impl
+    android.hardware.bluetooth.audio-impl
 
 # Biometrics
 PRODUCT_PACKAGES += \
