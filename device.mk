7bc81adab74fd594ec2a94b6149e25be216ee094
sajjad85gh <sajjad.ghaderi.1385@gmail.com>
2025-10-03 23:16:27 +0330

    Add LMODynamicWallpaper to PRODUCT_PACKAGES

--- device.mk
+++ device.mk
@@ -320,6 +320,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    LMODynamicWallpaper \
     FrameworksResOverlayZahedan \
     SettingsOverlayZahedan \
     SystemUIOverlayZahedan \
